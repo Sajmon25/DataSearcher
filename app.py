cce353e02b659eb190d8561d2d52c294ee6b0d34
Szymon Grzęda <szymon.grzedi@gmail.com>
2018-10-27 16:00:39 +0200

    Change IDE

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from searcher_ui import Ui_Searcher
 
+
 class MainWindow(Ui_Searcher):
+
     def __init__(self, dialog):
         Ui_Searcher.__init__(self)
         self.setupUi(dialog)
+
+        self.SearchButton.clicked.connect(self.search)
+
+    def search(self):
+        print("Searching...")
+
 
 if __name__ == '__main__':
 	app = QtWidgets.QApplication(sys.argv)
