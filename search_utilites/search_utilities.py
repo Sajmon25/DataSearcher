782590a1b9c7213e2cf0b7754ba5bd0c0c04e04e
Szymon Grzęda <szymon.grzedi@gmail.com>
2018-11-28 22:31:32 +0100

    Search printable value in a file

--- search_utilites/search_utilities.py
+++ search_utilites/search_utilities.py
@@ -10,25 +10,28 @@ class SearchUtility:
         folderList.sort()
         try:
             for d in folderList:
+                print("Befor " + d)
                 try:
                     fileList = instance_handler.listdir("/ORACLE/appl/fs1/EBSapps/appl/" + str(d) + "/12.0.0/reports/PL/")
                     for f in fileList:
                         if re.match('^(XX)|(XC)', f):
                             isValid = False
                             report_lines = ""
+                            print('File name: ' + f)
                             with instance_handler.open('/ORACLE/appl/fs1/EBSapps/appl/' + str(d) + '/12.0.0/reports/PL/' + str(f), "rb") as testFile:
                                 for line in nonprintable.split(testFile.read()):
                                     line = line.decode('UTF-8')
-                                    if (line.lower().find('B.BANK_ACCOUNT_ID'.lower()) != -1):
+                                    if (line.lower().find('FND FLEXIDVAL'.lower()) != -1):
                                         # report_lines += line
-                                        isValid = True
+                                        #isValid = True
 
-                                    if isValid:
+                                    #if isValid:
                                         # print(f)
-                                        print(report_lines)
-                                        msg.setPlaceholderText(report_lines)
+                                        print('Find line: ' + line)
+                                        msg.setPlaceholderText(line)
 
                 except Exception as e:
+                    print(d)
                     print(e)
 
         except Exception as e:
